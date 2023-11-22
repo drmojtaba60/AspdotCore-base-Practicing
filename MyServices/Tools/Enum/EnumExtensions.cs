0748b54db2e69cae37b5c7f644e9c8248264902d
shaghi <mojtaba.shagi@gmail.com>
2023-11-22 18:25:13 +0330

    Enum props with Reflection Extension

---
+++ MyServices/Tools/Enum/EnumExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace MyServices.Tools.EnumHelper
+{
+    public static class EnumExtensions
+        {
+            public static string ToDescription(this Enum value)
+            {
+                FieldInfo field;
+                DescriptionAttribute attribute;
+                string result;
+
+                field = value.GetType().GetField(value.ToString());
+                attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+                result = attribute != null ? attribute.Description : string.Empty;
+
+                return result;
+            }
+
+            public static bool HasDescription(this Enum someEnum)
+            {
+                return !string.IsNullOrEmpty(someEnum.ToDescription());
+            }
+
+            public static string ToDisplayName(this Enum value)
+            {
+                FieldInfo field;
+                DisplayNameAttribute attribute;
+                string result;
+
+                field = value.GetType().GetField(value.ToString());
+                attribute = (DisplayNameAttribute)Attribute.GetCustomAttribute(field, typeof(DisplayNameAttribute));
+                result = attribute != null ? attribute.DisplayName : string.Empty;
+
+                return result;
+            }
+
+            public static bool HasDisplayName(this Enum someEnum)
+            {
+                return !string.IsNullOrEmpty(someEnum.ToDisplayName());
+            }
+
+            public static int ToValue(this Enum someEnum)
+            {
+                return Convert.ToInt32(someEnum);
+            }
+
+        }
+    }
--- webapi-base/Controllers/EnumsController.cs
+++ webapi-base/Controllers/EnumsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Attributes;
+using MyServices.Tools.EnumHelper;
 using System.ComponentModel;
 using System.Reflection;
 using webapi_base.Enums;
@@ -70,6 +71,52 @@ namespace webapi_base.Controllers
                 
             }
             return Ok(categoryFullInfo);
+        }
+
+        [HttpGet("by-extension")]
+        public IActionResult GetCategoriesExtension()
+        {
+            var hasDescription = Category.Personnel.HasDescription();
+            var dataDescription = Category.Personnel.ToDescription();
+            Console.WriteLine($"hasDescription: {hasDescription}");
+            Console.WriteLine($"dataDescription: {dataDescription}");
+
+
+            var dataText = Category.Personnel.ToString();
+            Console.WriteLine($"dataText: {dataText}");
+
+            var dataValue = Category.Personnel.ToValue();
+            Console.WriteLine($"dataValue: {dataValue}");
+
+
+            var hasDIsplayName = Category.Personnel.HasDisplayName();
+            var dataDiaplayName = Category.Personnel.ToDisplayName();
+            Console.WriteLine($"hasDIsplayName: {hasDIsplayName}");
+            Console.WriteLine($"dataDiaplayName: {dataDiaplayName}");
+
+
+            var listText = Enum.GetValues(typeof(Category)).Cast<Category>().Select(d => d.ToString()).ToList();
+            var listValues = Enum.GetValues(typeof(Category)).Cast<int>().ToList();
+            var dictionary_with_value_and_description = Enum.GetValues(typeof(Category))
+                            .Cast<Category>()
+                            .ToDictionary(data => data.ToValue(), data => data.ToDescription());
+
+            var dictionary_with_value_and_display_name = Enum.GetValues(typeof(Category))
+                            .Cast<Category>()
+                            .ToDictionary(data => data.ToValue(), data => data.ToDisplayName());
+
+
+            return Ok(new
+            {
+                dataText,
+                dataValue,
+                hasDIsplayName,
+                dataDiaplayName,
+                listText,
+                listValues,
+                dictionary_with_value_and_description,
+                dictionary_with_value_and_display_name
+            });
         }
     }
 }
